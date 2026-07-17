a0e12f574a7071ae8e9f150e00fb895d618e99a1
John <jcandler2862@gmail.com>
2026-07-17 15:46:10 -0500

    Added scaffold with column. Added text_view requirement

--- app/src/main/java/com/example/hw2/MainActivity.kt
+++ app/src/main/java/com/example/hw2/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
 import com.example.hw2.ui.theme.HW2Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +26,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HW2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HW2App()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@PreviewScreenSizes
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HW2App() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = { CenterAlignedTopAppBar(title= { Text(stringResource(R.string.app_name)) }) }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(20.dp)
+        ){
+            Text_View(
+                modifier = Modifier
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    HW2Theme {
-        Greeting("Android")
-    }
+fun Text_View(modifier: Modifier = Modifier){
+    Text(
+        text = stringResource(R.string.txt_view),
+        textAlign = TextAlign.Center,
+        modifier = modifier.fillMaxWidth()
+    )
 }
